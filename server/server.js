fc905148f6c1a9989cc09aa779fb39816e48f06b
Sankar V <s_venkat_99@yahoo.co.uk>
2020-09-24 12:33:25 +0530

    GraphQL Schema and query mutation operations

--- server/server.js
+++ server/server.js
@@ -1,20 +1,74 @@
 var express = require('express')
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+var data = require('./data.json')
 
 //construct a schema, using GraphlQL schema language
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-  }
-`);
+
+type Book{
+    id: ID!
+    isbn: String!,
+    title: String!,
+    author: String!,
+    description: String!,
+    publisher: String!,
+    published_year: Int!
+}
+
+type Query {
+    quoteOfTheDay : String
+    random : Float!
+    rollThreeDice : [Int]
+    getBooks: [Book]!
+    getBook(id: ID!): Book
+}
+
+type Mutation {
+    addBook(
+        isbn: String!,
+        title: String!,
+        author: String!,
+        description: String,
+        publisher: String!,
+        published_year: Int!
+    ) : Book
+
+    updateBook(
+        id: ID!,
+        isbn: String!,
+        title: String!,
+        author: String!,
+        description: String,
+        publisher: String!,
+        published_year: Int!
+    ): Book
+
+    deleteBook(id: ID!): Book
+}
+
+type AddBookResponse {
+    success: Boolean!
+    message: String
+    book: Book
+}
+
+type UpdateBookResponse {
+    success: Boolean!
+    message: String
+    book: Book
+}
+
+type DeleteBookResponse {
+    success: Boolean!
+    message: String
+    book: Book
+}`);
 
 //the root provides a resolver function for each API Endpoint
 var root = {
-    quoteOfTheDay: () =>{
+    quoteOfTheDay: () => {
         return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
     },
     random: () => {
@@ -23,9 +77,60 @@ var root = {
     rollThreeDice: () => {
         return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
     },
+    getBooks: () => {
+        return data;
+    },
+    getBook: ({id}) => {
+        var book = data.find(function (e)
+            {
+                return e.id === id
+            }
+        );
+        return book;
+    },
+    addBook: ({isbn,title, author, description,publisher, published_year} ) => {
+        // Create a random id for our "database".
+        var id = require('crypto').randomBytes(10).toString('hex');
+        var book = {id, isbn, title, author, description, publisher, published_year};
+        console.log("Data before add::" + data.length)
+        data.push(book)
+        console.log("Data aftre add::" + data.length)
+        return book;
+    },
+    updateBook: ({id, isbn,title, author, description,publisher, published_year} ) => {
+        // Create a random id for our "database".
+        //var id = require('crypto').randomBytes(10).toString('hex');
+        
+        var book = {id, isbn, title, author, description, publisher, published_year};
+        data.push(book)
+        return book;
+    }
 };
 
+/** 
+mutation{
+  addBook(isbn: "9899917745", title:"Test", author:"Test Author", 
+    description:"Test description", publisher: "Mc Graw Hill",published_year: 1988){
+    id
+    isbn
+    description
+  }
+}
 
+mutation{
+  updateBook(
+    id:"6cfd4852fe359573c482", isbn:"9849017745",
+    title:"Modified title",
+    author:"Modified author",
+    description: "Modified description",
+    publisher: "Modified publisher",
+    published_year: 2000
+  ){
+    published_year
+    description
+  }
+}
+*/
 var app = express();
 app.use('/graphql', graphqlHTTP({
     schema: schema,
